f8da1d0fc324ceef3de5b5153b2b5a078b9783b5
sabrina-fontana <sabfon@hotmail.it>
2021-02-09 13:34:19 +0100

    inserite img

--- js/script.js
+++ js/script.js
@@ -7,10 +7,12 @@ data: {
 methods: {
   searchFilm: function() {
     let that = this;
-    console.log(this.searchInput)
+    if (this.searchInput === '') {
+      return this.arrayFilm = [];
+    }
     axios.get('https://api.themoviedb.org/3/search/movie?api_key=a2092b04d9693f9c0da61a113dc5f29a&query=' + this.searchInput)
     .then(function(resp) {
-      that.arrayFilm = resp.data.results
+      that.arrayFilm = resp.data.results;
       })
   }
 }
