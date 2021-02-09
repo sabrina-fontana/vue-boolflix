908f566fe1d0f1c4cb42a2377897508c98c34bbb
sabrina-fontana <sabfon@hotmail.it>
2021-02-09 11:54:45 +0100

    inizio prima milestone

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+el: '#app',
+data: {
+  searchInput: '',
+  arrayFilm: []
+},
+methods: {
+  searchFilm: function() {
+    let that = this;
+    console.log(this.searchInput)
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=a2092b04d9693f9c0da61a113dc5f29a&query=' + this.searchInput)
+    .then(function(resp) {
+      that.arrayFilm = resp.data.results
+      })
+  }
+}
+});
+Vue.config.devtools = true;
