4c54d79a81a5ef611b00d61d881c8d6246e03b77
sabrina-fontana <sabfon@hotmail.it>
2021-02-11 11:52:04 +0100

    mostro primi 5 attori

--- js/script.js
+++ js/script.js
@@ -5,31 +5,33 @@ data: {
   arrayFilm: [],
   arrayTV: [],
   resultId: 0,
-  actorsName: []
+  filmActorsName: [],
+  TVActorsName: []
 },
 methods: {
   search: function() {
-    let that = this;
     if (this.searchInput === '') {
       return [this.arrayFilm = [], this.arrayTV = []];
     }
+    this.searchFilm();
+    this.searchTV();
+  },
+  searchFilm: function() {
+    let that = this;
     // chiamata per film
     axios.get('https://api.themoviedb.org/3/search/movie?api_key=a2092b04d9693f9c0da61a113dc5f29a&query=' + this.searchInput)
     .then(function(resp) {
       that.arrayFilm = resp.data.results;
     })
+  },
+  searchTV: function() {
+    let that = this;
     // chiamata per serie tv
     axios.get('https://api.themoviedb.org/3/search/tv?api_key=a2092b04d9693f9c0da61a113dc5f29a&query=' + this.searchInput)
     .then(function(resp) {
       that.arrayTV = resp.data.results;
     })
   },
-  // getActors: function() {
-  //   axios.get('https://api.themoviedb.org/3/movie/' + this.resultId + '/credits?api_key=a2092b04d9693f9c0da61a113dc5f29a')
-  //   .then(function(resp) {
-  //     that.arrayFilm = resp.data.results;
-  //   })
-  // }
   resultImg: function(el) {
     if (el.poster_path === null) {
       return 'placeholder.png'
@@ -71,11 +73,7 @@ methods: {
     }
     return 'https://www.countryflags.io/' + language + '/flat/24.png';
   },
-  showInfo: function(className, index, el) {
-    let info = document.getElementsByClassName(className)[index];
-    info.classList.add('active');
-    this.resultId = el.id;
-    console.log(this.resultId)
+  getFilmActors: function() {
     let that = this;
     axios.get('https://api.themoviedb.org/3/movie/' + this.resultId + '/credits?api_key=a2092b04d9693f9c0da61a113dc5f29a')
     .then(function(resp) {
@@ -84,10 +82,44 @@ methods: {
       actors.forEach((element) => {
         names.push(element.name);
       })
-      that.actorsName = names;
-      console.log(that.actorsName)
+      that.filmActorsName = names;
+      console.log(that.filmActorsName.toString().replace(',', ', '))
     })
-
+  },
+  getTVActors: function() {
+    let that = this;
+    axios.get('https://api.themoviedb.org/3/tv/' + this.resultId + '/credits?api_key=a2092b04d9693f9c0da61a113dc5f29a')
+    .then(function(resp) {
+      let actors = resp.data.cast;
+      let names = [];
+      actors.forEach((element) => {
+        names.push(element.name);
+      })
+      that.TVActorsName = names;
+    })
+  },
+  showFilmActors: function() {
+    // solo i primi 5 attori
+    let actors = this.filmActorsName.slice(0, 5);
+    // ritorno una stringa
+    return actors.toString().replace(/,/g, ', ')
+  },
+  showTVActors: function() {
+    // solo i primi 5 attori
+    let actors = this.TVActorsName.slice(0, 5);
+    // ritorno una stringa
+    return actors.toString().replace(/,/g, ', ')
+  },
+  showInfo: function(className, index, el) {
+    let info = document.getElementsByClassName(className)[index];
+    info.classList.add('active');
+    this.resultId = el.id;
+    console.log(this.resultId)
+    if (className.includes('film')) {
+      this.getFilmActors();
+    } else if (className.includes('tv')) {
+      this.getTVActors();
+    }
   },
   hideInfo: function(className, index, el) {
     let info = document.getElementsByClassName(className)[index];
