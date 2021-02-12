87342f4a6c92e4760ff504a0fde371d290433049
sabrina-fontana <sabfon@hotmail.it>
2021-02-12 15:23:17 +0100

    milestone 6 completa

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ data: {
   TVGenres: [],
   movieActorsName: [],
   TVActorsName: [],
-  filmGenreSelected: 0,
+  movieGenreSelected: 0,
   TVGenreSelected: 0
 },
 mounted() {
@@ -26,38 +26,47 @@ mounted() {
   })
 },
 methods: {
-  search: function() {
-    if (this.searchInput === '') {
-      return [this.arrayMovie = [], this.arrayTV = []];
+  search: async function() {
+    console.log(this.movieGenreSelected)
+    if (this.searchInput === '' && this.movieGenreSelected === 0) {
+      return this.arrayMovie = [];
     }
-    this.searchMovie();
-    this.searchTV();
+    if (this.searchInput === '' && this.TVGenreSelected === 0) {
+      return this.arrayTV = [];
+    }
+    await this.searchMovie();
+    await this.searchTV();
+    if (this.movieGenreSelected !== 0) {
+      return this.filterMovie()
+    } else if (this.TVGenreSelected !== 0) {
+      return this.filterTV()
+    }
   },
-  searchMovie: function() {
+  searchMovie: async function() {
     this.arrayMovie = [];
     let that = this;
-    // chiamata per film (5 pagine)
-    for (var x = 1; x <= 5; x++) {
+    if (this.searchInput.length > 0) {
       return axios.get('https://api.themoviedb.org/3/search/movie?api_key=a2092b04d9693f9c0da61a113dc5f29a&page=' + 1 + '&query=' + this.searchInput)
       .then(function(resp) {
-        that.arrayMovie = [...that.arrayMovie, ...resp.data.results];
+        that.arrayMovie = resp.data.results;
       })
     }
+    return this.filterMovie()
   },
   searchTV: function() {
     this.arrayTV = [];
     let that = this;
-    // chiamata per serie tv (5 pagine)
-    for (var x = 1; x <= 5; x++) {
-      return axios.get('https://api.themoviedb.org/3/search/tv?api_key=a2092b04d9693f9c0da61a113dc5f29a&page=' + x + '&query=' + this.searchInput)
+    if (this.searchInput.length > 0) {
+      return axios.get('https://api.themoviedb.org/3/search/tv?api_key=a2092b04d9693f9c0da61a113dc5f29a&page=' + 1 + '&query=' + this.searchInput)
       .then(function(resp) {
-        that.arrayTV = [...that.arrayTV, ...resp.data.results];
+        that.arrayTV = resp.data.results;
       })
     }
+    return this.filterTV();
   },
   resultImg: function(el) {
     if (el.poster_path === null) {
-      return 'placeholder.png'
+      return 'img/placeholder.png'
     }
     return 'http://image.tmdb.org/t/p/w780' + el.poster_path;
   },
@@ -136,13 +145,22 @@ methods: {
     info.classList.remove('active');
   },
   filterMovie: async function() {
+    if (this.searchInput.length === 0) {
+      this.arrayMovie = [];
+      let that = this;
+      console.log(this.movieGenreSelected)
+      return axios.get('https://api.themoviedb.org/3/discover/movie?api_key=a2092b04d9693f9c0da61a113dc5f29a&with_genres=' + this.movieGenreSelected)
+      .then(function(resp) {
+        return that.arrayMovie = resp.data.results;
+      })
+    }
     await this.searchMovie();
     let filteredMovies = [];
-      if (this.filmGenreSelected  === 0) {
+      if (this.movieGenreSelected  === 0) {
         return this.arrayMovie
       } else {
         filteredMovies = this.arrayMovie.filter((element) => {
-          if (element.genre_ids.includes(this.filmGenreSelected)) {
+          if (element.genre_ids.includes(this.movieGenreSelected)) {
             return element;
           }
         })
@@ -150,9 +168,17 @@ methods: {
       }
   },
   filterTV: async function() {
+    if (this.searchInput.length === 0) {
+      this.arrayTV = [];
+      let that = this;
+      return axios.get('https://api.themoviedb.org/3/discover/tv?api_key=a2092b04d9693f9c0da61a113dc5f29a&with_genres=' + this.TVGenreSelected)
+      .then(function(resp) {
+        return that.arrayTV = resp.data.results;
+      })
+    }
     await this.searchTV();
     let filteredTV = [];
-      if (this.TVGenreSelected  === 0) {
+      if (this.TVGenreSelected === 0) {
         return this.arrayTV
       } else {
         filteredTV = this.arrayTV.filter((element) => {
@@ -162,6 +188,9 @@ methods: {
         })
         return this.arrayTV = filteredTV
       }
+  },
+  noResults: function(array, typeGenre) {
+    return array.length === 0 && typeGenre !== 0;
   }
   }
 });
